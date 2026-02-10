f272b85c9f8f89fd729ac9f984a29c07b1d38e8b
lotus <temuulentemka929@gmail.com>
2026-02-10 16:46:30 +0800

    undes

--- elearn/users/models.py
+++ elearn/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_date = models.DateField(auto_now_add=True, null=True)
+    last_updated_date = models.DateField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+    
