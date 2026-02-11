729eb2c76f480dba941842a7f6d0cb039c619d4e
lotus <temuulentemka929@gmail.com>
2026-02-11 15:16:58 +0800

    1

--- elearn/users/models.py
+++ elearn/users/models.py
@@ -10,4 +10,8 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-    
+
+
+class UserModel(models.Model):
+    user_name = models.CharField("NickName", max_length=255, null=True, blank=True)
+    email =
