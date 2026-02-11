7aadd21c9f0b6aaf7b016a35c58e87130c5bb239
lotus <temuulentemka929@gmail.com>
2026-02-11 15:36:08 +0800

    test

--- elearn/users/models.py
+++ elearn/users/models.py
@@ -12,6 +12,15 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class UserModel(models.Model):
-    user_name = models.CharField("NickName", max_length=255, null=True, blank=True)
-    email =
+class User(BaseModel):
+    username = models.CharField("NickName", max_length=255, null=True, blank=True)
+    email = models.EmailField(verbose_name="Email", max_length = 255, null=True, blank=True)
+    phone = models.IntegerField(verbose_name="Phone", max_length= 8, null=True, blank=True)
+    password = models.CharField(verbose_name="Password", max_length = 16, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "User"
+        
+    def __str__(self) -> str:
+        return self.username
--- elearn/users/views.py
+++ elearn/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import os
+import json
+import logging
 
+from django.http import JsonResponse
+
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth import login
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import make_password
+
+from users.models import User
 # Create your views here.
+
+def ping(request):
+    data = {}
+    data['build_mode'] = os.environ.get("BUILD_MODE")
+    data['build_date'] = os.environ.get("BUILD_DATE")
+    data['version'] = os.environ.get("IMAGE_VERSION")
+    data['app'] = "core"
+    return JsonResponse(data)
+
+logger = logging.getLogger(__name__)
