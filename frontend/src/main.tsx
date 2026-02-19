e1aeb81492b33eb807247e7c84161db27e5789a8
lotus <temuulentemka929@gmail.com>
2026-02-19 20:43:30 +0800

    api d asuudal garsangit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,9 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
